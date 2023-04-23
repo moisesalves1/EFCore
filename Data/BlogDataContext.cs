090e8002397c96cd22a79da42ea8f7bf99b26bce
Moises Alves <moisesalves1@gmail.com>
2023-04-23 18:08:47 -0300

    DbSet

---
+++ Data/BlogDataContext.cs
@@ -0,0 +1,11 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Blog.Data;
+
+namespace Blog 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var ctx = new BlogDataContext();
+            ctx.Categories;
+        }
+    }
+}
