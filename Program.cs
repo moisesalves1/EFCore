227b057622549bcfc8ffa283932998396dc4e4dd
Moises Alves <moisesalves1@gmail.com>
2023-04-30 01:58:24 -0300

    Criando uma migration

--- Program.cs
+++ Program.cs
@@ -9,35 +9,6 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            using var context = new BlogDataContext();
-
-            // context.Users.Add(new User{
-            //    Bio = "9X microsoft MVP",
-            //    Email = "andre@balta.io",
-            //    Image = "https://balta.io",
-            //    Name = "Andre Baltiei",
-            //    PasswordHash = "1234",
-            //    Slug = "andre-baltieri"
-            // });
-
-            var user = context.Users.FirstOrDefault();
-            var post = new Post{
-               Author = user,
-               Body = "Meu artigo",
-               Category = new Category
-               {
-                    Name = "Backend",
-                    Slug = "backend"
-               },
-               CreateDate = System.DateTime.Now,
-               Slug = "meu-artigo",
-               Summary = "Neste artigo vamos conferir...",
-               Title = "Meu artigo"
-            };
-
-            context.Posts.Add(post);
-            
-            context.SaveChanges();
 
         }
     }
