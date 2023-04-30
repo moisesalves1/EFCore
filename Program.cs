3b7c30d37786c4957dab80e74d03db9f90a8c557
Moises Alves <moisesalves1@gmail.com>
2023-04-29 22:38:20 -0300

    Includ

--- Program.cs
+++ Program.cs
@@ -12,38 +12,47 @@ namespace Blog
             using var context = new BlogDataContext();
 
 
-            var user = new User
-            {
-                Name = "André Baltieri",
-                Slug = "andrebaltieri",
-                Email = "andre@balta.io",
-                Bio = "9x Microsoft MVP",
-                Image = "https://balta.io",
-                PasswordHash = "123098457"
-            };
+            // var user = new User
+            // {
+            //     Name = "André Baltieri",
+            //     Slug = "andrebaltieri",
+            //     Email = "andre@balta.io",
+            //     Bio = "9x Microsoft MVP",
+            //     Image = "https://balta.io",
+            //     PasswordHash = "123098457"
+            // };
 
-            var category = new Category
-            {
-                Name = "Backend",
-                Slug = "backend"
-            };
+            // var category = new Category
+            // {
+            //     Name = "Backend",
+            //     Slug = "backend"
+            // };
 
-            var post = new Post
-            {
-                Author = user,
-                Category = category,
-                Body = "<p>Hello World</P",
-                Slug = "comecando-com-ef-core",
-                Summary = "Neste artigo vamos aprender EF Core",
-                Title = "Começando com EF Core",
-                CreateDate = DateTime.Now,
-                LastUpdateDate = DateTime.Now
+            // var post = new Post
+            // {
+            //     Author = user,
+            //     Category = category,
+            //     Body = "<p>Hello World</P",
+            //     Slug = "comecando-com-ef-core",
+            //     Summary = "Neste artigo vamos aprender EF Core",
+            //     Title = "Começando com EF Core",
+            //     CreateDate = DateTime.Now,
+            //     LastUpdateDate = DateTime.Now
 
-            };
+            // };
 
-            context.Posts.Add(post);
-            context.SaveChanges();
+            // context.Posts.Add(post);
+            // context.SaveChanges();
 
+            var posts = context
+                .Posts
+                .AsNoTracking()
+                .Include(x=>x.Author)
+                .OrderByDescending(x=>x.LastUpdateDate)
+                .ToList();
+
+            foreach (var post in posts)
+                Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
 
 
         }
