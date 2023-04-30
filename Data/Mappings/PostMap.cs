ccd03602a546404a4dd00545c5d0fa54aa792ae5
Moises Alves <moisesalves1@gmail.com>
2023-04-30 01:10:39 -0300

    Default Values

---
+++ Data/Mappings/PostMap.cs
@@ -0,0 +1,32 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class PostMap : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.ToTable("Post");
+
+            builder.HasKey(x=>x.Id);
+
+            builder.Property(x=>x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.LastUpdateDate)
+                .IsRequired()
+                .HasColumnName("LastUpdateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasDefaultValueSql("GETDATE()");
+                // .HasDefaultValue(DateTime.Now.ToUniversalTime());
+
+            builder.HasIndex(x=>x.Slug, "IX_Post_Slug")
+                .IsUnique();
+                
+        
+        }
+    }
+}
