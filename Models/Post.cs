50ed5acba369bdca742196bdc4042f92625cdcb6
Moises Alves <moisesalves1@gmail.com>
2023-04-30 01:04:20 -0300

    Iniciando o mapeamento do User

--- Models/Post.cs
+++ Models/Post.cs
@@ -7,19 +7,15 @@ namespace Blog.Models {
     public class Post 
     {
         public int Id {get; set;}
-
-        [ForeignKey("CategoryId")]
-        public int CategoryId {get; set;}
-        public Category Category {get; set;}
-
-        [ForeignKey("AuthorId")]
-        public int AuthorId {get; set;}
-        public User Author { get; set; }
         public string Title {get; set;}
         public string Summary {get; set;}
         public string Body {get; set;}
         public string Slug {get; set;}
         public DateTime CreateDate {get; set;}
         public DateTime LastUpdateDate {get; set;}
+
+        public Category Categories { get; set; }
+        public User Author { get; set; }
+        public List<Tag> Tags { get; set; }
     }
 }
--- Models/PostTag.cs
+++
@@ -1,7 +0,0 @@
-namespace Blog.Models {
-    public class PostTag 
-    {
-        public int PostId {get; set;}
-        public int TagId {get; set;}
-    }
-}
