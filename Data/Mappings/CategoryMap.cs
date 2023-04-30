1bdf47aed01b56de63bd8d86ee684db06e18002a
Moises Alves <moisesalves1@gmail.com>
2023-04-29 23:13:21 -0300

    Iniciando o mapeamento

---
+++ Data/Mappings/CategoryMap.cs
@@ -0,0 +1,16 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            // Tabela
+            builder.ToTable("Category");
+
+        }
+    }
+}
