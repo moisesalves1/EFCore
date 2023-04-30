525181c050efca06fe213329edfafec02097cddd
Moises Alves <moisesalves1@gmail.com>
2023-04-30 01:17:16 -0300

    OneToMany

--- Data/Mappings/PostMap.cs
+++ Data/Mappings/PostMap.cs
@@ -26,6 +26,10 @@ namespace Blog.Data.Mappings
             builder.HasIndex(x=>x.Slug, "IX_Post_Slug")
                 .IsUnique();
                 
+            builder.HasOne(x => x.Author)
+                .WithMany(x=>x.Posts)
+                .HasConstraintName("FK_Post_Author")
+                .OnDelete(DeleteBehavior.Cascade);
         
         }
     }
