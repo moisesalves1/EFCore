98010e73574559468b123dc510bb6cee2086c063
Moises Alves <moisesalves1@gmail.com>
2023-04-29 22:04:33 -0300

    Table, Key e Identity

--- Program.cs
+++ Program.cs
@@ -11,55 +11,6 @@ namespace Blog
         {
             using (var context = new BlogDataContext())
             {
-                // var tag = new Tag {Name = ".NET Core", Slug="netcore"};
-                // context.Tags.Add(tag);
-                // context.SaveChanges();
-
-                // var tag = context.Tags.FirstOrDefault(x=>x.Id == 2);
-                // tag.Name = ".NET";
-                // tag.Slug = "dotnet";
-
-                // context.Update(tag);
-                // context.SaveChanges();
-
-                // var tag = context.Tags.FirstOrDefault(x=>x.Id == 2);
-
-                // context.Remove(tag);
-                // context.SaveChanges();
-
-                // var tags = context
-                //     .Tags
-                //     .ToList();
-
-                // foreach( var tag in tags){
-                //     Console.WriteLine(tag.Name);
-                // }
-
-                // var tags = context
-                //     .Tags
-                //     .AsNoTracking()
-                //     .ToList();
-
-                //     foreach (var tag in tags){
-                //         Console.WriteLine(tag.Name);
-                //     }
-
-                // var tag = context
-                //     .Tags
-                //     .AsNoTracking() //Remover
-                //     .FirstOrDefault(x=>x.Id == 1002);
-                // tag.Name = ".NET";
-                // tag.Slug = "dotnet";
-
-                // context.Update(tag);
-                // context.SaveChanges();
-
-                var tag = context
-                    .Tags
-                    .AsNoTracking()
-                    .FirstOrDefault(x => x.Id == 29);
-
-                Console.WriteLine(tag?.Name);
 
             }
 
