2ccd96aa67bce1a3f3e60c0019e56f82ebacfada
Moises Alves <moisesalves1@gmail.com>
2023-04-29 22:48:34 -0300

    Loig

--- Program.cs
+++ Program.cs
@@ -48,11 +48,12 @@ namespace Blog
                 .Posts
                 .AsNoTracking()
                 .Include(x=>x.Author)
+                .Include(x=>x.Category)
                 .OrderByDescending(x=>x.LastUpdateDate)
                 .ToList();
 
             foreach (var post in posts)
-                Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
+                Console.WriteLine($"{post.Title} escrito por {post.Author?.Name} em {post.Category?.Name}");
 
 
         }
